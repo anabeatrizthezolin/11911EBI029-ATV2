10b1350e03250bf9d904df5fefcb3dc28d324d5f
anabeatriz <thezolin00@gmail.com>
2022-06-13 14:03:26 -0300

    Atualização

--- src/main.c
+++ src/main.c
@@ -29,7 +29,6 @@
 #define STM32_GPIOA_MODER   (STM32_GPIOA_BASE+STM32_GPIO_MODER_OFFSET)
 #define STM32_GPIOA_OTYPER  (STM32_GPIOA_BASE+STM32_GPIO_OTYPER_OFFSET)
 #define STM32_GPIOA_PUPDR   (STM32_GPIOA_BASE+STM32_GPIO_PUPDR_OFFSET)
-#define STM32_GPIOA_ODR     (STM32_GPIOA_BASE+STM32_GPIO_ODR_OFFSET)
 #define STM32_GPIOA_BSRR    (STM32_GPIOA_BASE+STM32_GPIO_BSRR_OFFSET)
 #define STM32_GPIOA_IDR     (STM32_GPIOA_BASE+STM32_GPIO_IDR_OFFSET)
 
@@ -75,12 +74,10 @@
 #define GPIO_BSRR_SET(n)    (1 << (n))
 #define GPIO_BSRR_RST(n)    (1 << (n + 16))
 
-static const char fw_version[] = {'V','1','.','0'};
 static uint32_t led_status; 			// armazena o estado do LED
 
 int main(int argc, char *argv[]){
     uint32_t reg;
-    uint32_t regA;
 	uint32_t LED_DELAY;
     // Ponteiros para registradores 
     uint32_t *pRCC_AHB1ENR  = (uint32_t*)STM32_RCC_AHB1ENR;
@@ -89,55 +86,46 @@ int main(int argc, char *argv[]){
     uint32_t *pGPIOC_PUPDR  = (uint32_t*)STM32_GPIOC_PUPDR;
     uint32_t *pGPIOC_BSRR   = (uint32_t*)STM32_GPIOC_BSRR;
 
-    uint32_t *pRCC_AHB1ENRA  = (uint32_t*)STM32_RCC_AHB1ENR;
     uint32_t *pGPIOA_MODER   = (uint32_t*)STM32_GPIOA_MODER;
     uint32_t *pGPIOA_IDR     = (uint32_t*)STM32_GPIOA_IDR;
     uint32_t *pGPIOA_PUPDR   = (uint32_t*)STM32_GPIOA_PUPDR;
-    uint32_t *pGPIOA_BSRR    = (uint32_t*)STM32_GPIOA_BSRR;
 
     // Habilitar o clock GPIOC
     reg = *pRCC_AHB1ENR;
-    reg |= RCC_AHB1ENR_GPIOCEN; // reg = reg | (1 << 2)
+    reg |= (RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOAEN); 
     *pRCC_AHB1ENR = reg;
 
     // Configura PC13 como saida pull-up off e pull-down off 
     reg = *pGPIOC_MODER;
-    reg &= ~(GPIO_MODER13_MASK);
-    reg |= (GPIO_MODER_OUTPUT << GPIO_MODER13_SHIFT);
+    reg &= ~(GPIO_MODER13_MASK); // zera os bits nas posições 27 e 26
+    reg |= (GPIO_MODER_OUTPUT << GPIO_MODER13_SHIFT); // 
     *pGPIOC_MODER = reg;
     
     reg = *pGPIOC_OTYPER;
-    reg &= ~(GPIO_OT13_MASK);
+    reg &= ~(GPIO_OT13_MASK);  // zera os bits nas posições 27 e 26
     reg |= (GPIO_OTYPER_PP << GPIO_OT13_SHIFT);
     *pGPIOC_OTYPER = reg;
     
     reg = *pGPIOC_PUPDR;
-    reg &= ~(GPIO_PUPDR13_MASK);
+    reg &= ~(GPIO_PUPDR13_MASK);  // zera os bits nas posições 27 e 26
     reg |= (GPIO_PUPDR_NONE << GPIO_PUPDR13_SHIFT);
     *pGPIOC_PUPDR = reg;
 
-    // Habilitar o clock GPIOA
-    regA = *pRCC_AHB1ENRA;
-    regA |= RCC_AHB1ENR_GPIOAEN; 
-    *pRCC_AHB1ENRA = regA;
-
     // Configura PA0 como entrada pull-up
-    
-    regA = *pGPIOA_PUPDR;
-    regA &= ~(GPIO_PUPDR0_MASK);
-    regA |= (GPIO_PUPDR_PULLUP << GPIO_PUPDR0_SHIFT);
-    *pGPIOA_PUPDR = regA;
 
-    regA = *pGPIOA_MODER;
-    regA &= ~(GPIO_MODER0_MASK);
-    regA |= (GPIO_MODER_INPUT << GPIO_MODER0_SHIFT);
-    *pGPIOA_MODER = regA;
-    
-    *pGPIOA_IDR &= RCC_AHB1ENR_GPIOAEN;
+    reg = *pGPIOA_MODER;
+    reg &= ~(GPIO_MODER0_MASK);  // zera os bits nas posições 0 e 1
+    reg |= (GPIO_MODER_INPUT << GPIO_MODER0_SHIFT); // muda os bits 1 e 0 para 0 e 0 (já estão zerados)
+    *pGPIOA_MODER = reg;
+
+    reg = *pGPIOA_PUPDR;
+    reg &= ~(GPIO_PUPDR0_MASK);  // zera os bits nas posições 0 e 1
+    reg |= (GPIO_PUPDR_PULLUP << GPIO_PUPDR0_SHIFT); // muda os bits 1 e 0 para 0 e 1
+    *pGPIOA_PUPDR = reg;
 
     while(1){
-        if(pGPIOA_IDR){
-            LED_DELAY = 200000;
+        if((*pGPIOA_IDR & 1)){ // zera todos os bits e verifica só o ultimo
+            LED_DELAY = 500000;
         }
         else{
             LED_DELAY = 50000;
